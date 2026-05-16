c8f2fa72cf0d6aed379a61ec259a340b3462bade
Hediye Sert <hediye-sert@hotmail.com>
2026-05-16 13:41:34 +0300

    Initial Commit

---
+++ HealthDashboard/HealthDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthDashboardApp.swift
+//  HealthDashboard
+//
+//  Created by hediye sert on 16.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct HealthDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
